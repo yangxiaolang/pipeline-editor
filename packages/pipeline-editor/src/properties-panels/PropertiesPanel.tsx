799af5c7b343f72aa3cf418a574d665741b01654
Haoxin Yang <hxyang@alauda.io>
2023-04-26 14:37:36 +0800

    feat: add form common i18n

--- packages/pipeline-editor/src/properties-panels/PropertiesPanel.tsx
+++ packages/pipeline-editor/src/properties-panels/PropertiesPanel.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { useCallback, useMemo } from "react";
+import { useMemo } from "react";
 
 import Form, { UiSchema, Widget, AjvError } from "@rjsf/core";
 import { produce } from "immer";
@@ -93,13 +93,13 @@ export function PropertiesPanel({
 
   const translateTitleAndDes = processField(["title", "description"]);
 
-  // const schemaTranslated = useMemo(
-  //   () =>
-  //     translateTitleAndDes(schema, (value) =>
-  //       intl.formatMessage({ id: value })
-  //     ),
-  //   [intl, schema, translateTitleAndDes]
-  // );
+  const schemaTranslated = useMemo(
+    () =>
+      translateTitleAndDes(schema, (value) =>
+        intl.formatMessage({ id: value })
+      ),
+    [intl, schema, translateTitleAndDes]
+  );
 
   if (schema === undefined) {
     return (
@@ -109,15 +109,10 @@ export function PropertiesPanel({
     );
   }
 
-
-
-  // console.log(schema);
-  // console.log(schemaTranslated);
-
   let uiSchema: UiSchema = {};
-  for (const field in schema.properties) {
+  for (const field in schemaTranslated.properties) {
     uiSchema[field] = {};
-    const properties = schema.properties[field];
+    const properties = schemaTranslated.properties[field];
     if (properties.type === "object") {
       for (const subField in properties.properties) {
         const subProps = properties.properties[subField];
@@ -133,17 +128,18 @@ export function PropertiesPanel({
 
   uiSchema = {
     ...uiSchema,
-    ...schema.uihints,
+    ...schemaTranslated.uihints,
   };
 
   return (
     <Form
       formData={data}
       uiSchema={uiSchema}
-      schema={schema as any}
+      schema={schemaTranslated as any}
       onChange={(e) => {
         const newFormData = e.formData;
-        const params = schema.properties?.component_parameters?.properties;
+        const params =
+          schemaTranslated.properties?.component_parameters?.properties;
         for (const field in params) {
           if (params[field].oneOf) {
             for (const option of params[field].oneOf) {
